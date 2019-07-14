7683ad52cd1d91d978e450f1731ee87b10c1dd4f
Arthur Dujardin <art.dujardin@hotmail.fr>
2019-07-14 16:20:08 +0200

    ajout fonctions ply.py de conversion en array

--- fimage.py
+++ fimage.py
@@ -1,8 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 14 10:17:54 2019
+
+@author: Cédric Perion | Arthur Dujardin
+"""
+
+
 import  numpy as np
 
 def fimage(F, M, R, S) :
-    k=np.ndarray([0,0,1])
-    R_inv=np.linalg.inv(R)
-    top = k.transpose().dot(F).dot(R).dot(M - S)
+    k = np.ndarray([0,0,1])
+    R_inv = np.linalg.inv(R)
+    top = k.dot(F).dot(R).dot(M - S)
     bottom = k.transpose.dot(R).dot(M -S)
     return F - top/bottom
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- io/ply.py
+++ io/ply.py
@@ -1,15 +1,44 @@
-
+# -*- coding: utf-8 -*-
 """
-@author Cedric Perion | Arthur Dujardin
+Created on Sun Jul 14 10:17:54 2019
+
+@author: Cédric Perion | Arthur Dujardin
 """
 
+
+import numpy as np
 import plyfile
 
 def readply(fname):
     """ Reads data from the ply file"""
-    plydata=plyfile.PlyData.read(fname)
+    plydata = plyfile.PlyData.read(fname)
+    return plydata
 
 def writeply(data, fname):
-    """ Writes data to the plyfile"""
+    """
+    Writes data to the plyfile
+    @param data : the data to convert in ply
+    @paramtype data : np.array
+    """
     el = plyfile.PlyElement.describe(data, 'vertex')
     plyfile.PlyData([el], text=True).write(fname)
+    return data
+
+def convertPlyArray(plydata):
+    """
+    """
+    data = np.array([plydata.elements[0].data['x']])
+
+    return data
+
+
+if __name__ == "__main__" :
+    fname = "el.ply"
+    plydata = readply(fname)
+    print("The data from ", plydata, " is :\n", convertPlyArray(plydata))
+    
+    
+    
+    
+    
+    
