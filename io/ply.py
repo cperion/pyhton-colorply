a42b7dab7e168ef708b74fc0a6082bfe64fddba4
cperion <cedric.perion@ensg.eu>
2019-07-14 12:18:29 +0200

    ply read and write functions

---
+++ io/ply.py
@@ -0,0 +1,17 @@
+
+"""
+@author Cedric Perion | Arthur Dujardin
+"""
+
+import plyfile
+
+def readply(fname):
+    """ Reads data from the ply file"""
+    plydata=plyfile.PlyData.read(fname)
+
+def writeply(data, fname):
+    """ Writes data to the plyfile"""
+    el = plyfile.PlyElement.describe(data, 'vertex')
+    plyfile.PlyData([el], text=True).write(fname)
+    
+
