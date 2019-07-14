d709694d982443b2f952bcf55980e8322be382f6
cperion <cedric.perion@ensg.eu>
2019-07-14 16:18:01 +0200

    update

--- fimage.py
+++ fimage.py
@@ -1,8 +1,13 @@
 import  numpy as np
 
 def fimage(F, M, R, S) :
+    """ Compute the image formula : 
+        
+    """
     k=np.ndarray([0,0,1])
     R_inv=np.linalg.inv(R)
     top = k.transpose().dot(F).dot(R).dot(M - S)
     bottom = k.transpose.dot(R).dot(M -S)
     return F - top/bottom
+
+def radialStd 
--- util/util.py
+++ util/util.py
@@ -1,3 +1,6 @@
 import plyfile
 
-def 
+def toascii(inputFile, outPutfile) :
+    el = plyfile.PlyData.read(inputFile)
+    el2 = plyfile.PlyElement.describe(el.elements[0].data, 'vertex')
+    plyfile.PlyData([el2], text=True).write(outPutfile)
