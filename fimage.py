ec97ce1b9b208c9398eb3b78fe73b0eeef17631a
cperion <cedric.perion@ensg.eu>
2019-07-14 17:32:58 +0200

    radial distorsion formula

--- fimage.py
+++ fimage.py
@@ -10,4 +10,19 @@ def fimage(F, M, R, S) :
     bottom = k.transpose.dot(R).dot(M -S)
     return F - top/bottom
 
-def radialStd 
+def radialStd(point, pps, a, b, c) :
+    """ Corrects the postion of the point according to the standard radial distorsion model"""
+    r = np.linalg.norm(point-pps)
+    rsquared= r*r
+    """ We use HORNEEERRRRRRRR to evluate ar2+br4+cr6"""
+    poly = c
+    poly = poly*rsquared + b
+    poly = poly*rsquared + a
+    poly = poly*rsquared
+    """ HORNEEEEERRRRR DONE """
+    dr = poly*(point-pps) # correction vector
+    return point + dr
+
+def cimage(F, M, R, S, pps, a, b, c,) :
+    return radialStd(fimage(F, M, R, S), pps, a, b ,c)
+
--- util/util.py
+++ util/util.py
@@ -4,3 +4,5 @@ def toascii(inputFile, outPutfile) :
     el = plyfile.PlyData.read(inputFile)
     el2 = plyfile.PlyElement.describe(el.elements[0].data, 'vertex')
     plyfile.PlyData([el2], text=True).write(outPutfile)
+
+toascii('NuageGREEN.ply', 'elc.ply')
